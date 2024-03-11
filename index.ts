da990264d12256a7614045fb278e50ce830b79b3
tsomani <tabreek.somani@gmail.com>
2024-03-11 15:22:24 -0700

    Initial Commit

---
+++ index.ts
@@ -0,0 +1,13 @@
+import express, { Request, Response } from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!');
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+})
